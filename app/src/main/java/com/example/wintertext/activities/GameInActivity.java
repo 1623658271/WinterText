8c5de6f07bf862f2883992b2fa337539c0696107
1623658271 <1623658271@qq.com>
2022-02-16 00:29:15 +0800

    寒假考核-更改

--- app/src/main/java/com/example/wintertext/activities/GameInActivity.java
+++ app/src/main/java/com/example/wintertext/activities/GameInActivity.java
@@ -241,11 +241,11 @@ public class GameInActivity extends AppCompatActivity {
     //游戏
     public  void gamePlay(boolean SI,boolean SHENG,boolean JING,boolean BING){
             if(SI){
-                yong_en.setAttack(yong_en.getAttack()*3);
-                yong_en.setDefense(yong_en.getDefense()*3);
-                yong_en.setLife(yong_en.getLife()*3);
-                yong_en.setStrike(yong_en.getStrike()*3);
-                yong_en.setSteal(yong_en.getSteal()*3);
+                yong_en.setAttack(yong_en.getAttack()*2);
+                yong_en.setDefense(yong_en.getDefense()*2);
+                yong_en.setLife(yong_en.getLife()*2);
+                yong_en.setStrike(yong_en.getStrike()*2);
+                yong_en.setSteal(yong_en.getSteal()*2);
                 yong_en_life.setText(String.valueOf(yong_en.getLife()));
             }
             if(SHENG){
